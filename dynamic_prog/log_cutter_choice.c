a9539841f8d9d6a1330ec53707463efd51dbf568
Barun Halder <bhalder@bhalder@andrew.cmu.edu>
2016-01-15 11:29:03 -0500

    Dynamic-2

---
+++ dynamic_prog/log_cutter_choice.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <time.h>
+
+clock_t start, end;
+double cpu_time_used;
+
+#define MAX 200
+#define M( x, y ) ( (x) > (y) > (x) : (y) )
+
+int BEST[MAX];
+
+int size[MAX] = {1, 2, 3, 5, 6, 9};
+int price[MAX] = { 3, 1, 6, 9, 2, 10 };
+int choice[MAX];
+
+int get_max_cut( int cut ) {
+    int max_index = 0;
+    int max_value = 0; 
+    int i = 0;
+
+    for( i = 0 ; i < cut; i++ ) {
+        if( max_value < (BEST[ cut - i ] + price[ i ] ) ) {
+            max_value = (BEST[ cut - i ] + price[ i ] );
+            max_index = i;
+        }
+    }
+
+    BEST[cut] = max_value;
+    return max_index;
+}
+
+int get_max_cut_value ( int length ) {
+
+    int value = 0;
+    int i = 0;
+
+    for( i = 0 ; i < length; i++ ) {
+        int ch = get_max_cut( i ) ;     
+        choice[i] = ch;
+    }
+
+    return BEST[length-1];
+}
+
+int main() {
+
+    int i = 0;
+    for( i = 0; i < MAX; i++ ) {
+        BEST[i] = 0;
+    }
+
+    start = clock();
+    int max_value =  get_max_cut_value(18);
+    end = clock();
+
+    cpu_time_used = ( ( double ) ( end - start ) ) / CLOCKS_PER_SEC; 
+    printf("\nChosen log sizes are\n");
+    int left = 18;
+    while( left != 0 ) {
+        printf("\n%d", size[ choice[left] ] );
+        left -= size[ choice[ left ] ];
+    }
+
+    printf( "\nResult is %d\nTime taken is %f\n", max_value, cpu_time_used );
+    return 0;
+}
